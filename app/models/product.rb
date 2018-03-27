3d60c9560c4e238010806ff333ceb2a75a8a530f
pedrogrande <pete@coderfactory.com>
2018-03-27 17:45:03 +1100

    update seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :seller_profile
 
-  validates :name, :description, :image, :price, :num_in_stock, presence: true
+  validates :name, :description, :price, :num_in_stock, presence: true
   validates :price, :num_in_stock, numericality: { greater_than_or_equal_to: 0 }
 
   mount_uploader :image, ProductImageUploader
